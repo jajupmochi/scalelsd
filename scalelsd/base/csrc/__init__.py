c064eb31d783e984d5ab20f1ff71b2d3588c6a96
Nan Xue <xuenan@ieee.org>
2025-06-27 11:06:53 +0800

    Update __init__.py

--- scalelsd/base/csrc/__init__.py
+++ scalelsd/base/csrc/__init__.py
@@ -13,7 +13,4 @@ try:
 except:
     _C = None
 
-_C = load(name='_C', sources=[osp.join(__this__,'binding.cpp'), osp.join(__this__,'linesegment.cu')])
 __all__ = ["_C"]
-
-#_C = load(name='base._C', sources=['lltm_cuda.cpp', 'lltm_cuda_kernel.cu'])
